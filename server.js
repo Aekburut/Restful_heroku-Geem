1840cb892ee2df2133fb71504a836f7cac928903
Aekburut Rawangngan <egburut@gmail.com>
2018-11-12 14:45:43 +0700

    แก้ไขต่อ

--- server.js
+++ server.js
@@ -24,31 +24,31 @@ app.get('/api/json', function (req, res) {
 });
 
 //การทำงานของ Products
-app.get('/api/products', db.getAllProducts);
-app.get('/api/products/:id', db.getProductsByID);
-app.post('/api/products', db.InsertProducts);
-app.put('/api/products/:id', db.UpdateProducts);
-app.delete('/api/products/:id', db.DeleteProducts);
+            app.get('/api/products', db.getAllProducts);
+            app.get('/api/products/:id', db.getProductsByID);
+            app.post('/api/products', db.InsertProducts);
+            app.put('/api/products/:id', db.UpdateProducts);
+            app.delete('/api/products/:id', db.DeleteProducts);
 
 //การทำงานของ users
-app.get('/api/users', db.getUsers);
-app.get('/api/users/:id', db.getUsersByID);
-app.post('/api/users', db.InsertUsers);
-app.put('/api/users/:id', db.UpdateUsers);
-app.delete('/api/users/:id', db.DeleteUsers);
+            app.get('/api/users', db.getUsers);
+            app.get('/api/users/:id', db.getUsersByID);
+            app.post('/api/users', db.InsertUsers);
+            app.put('/api/users/:id', db.UpdateUsers);
+            app.delete('/api/users/:id', db.DeleteUsers);
 
 //การทำงานของ Purchase_item
-app.get('/api/purchase_items', db.getPurchase_items);
-app.get('/api/purchase_items/:id', db.getPurchase_itemsByID);
-app.post('/api/purchase_items', db.InsertPurchase_items);
-app.put('/api/purchase_items/:id', db.UpdatePurchase_items);
-app.delete('/api/purchase_items/:id', db.DeletePurchase_items);
+            app.get('/api/purchase_items', db.getPurchase_items);
+            app.get('/api/purchase_items/:id', db.getPurchase_itemsByID);
+            app.post('/api/purchase_items', db.InsertPurchase_items);
+            app.put('/api/purchase_items/:id', db.UpdatePurchase_items);
+            app.delete('/api/purchase_items/:id', db.DeletePurchase_items);
 //การทำงานของ Purchase
-app.get('/api/purchase', db.getPurchase);
-app.get('/api/purchase/:id', db.getPurchaseByID);
-app.post('/api/purchase', db.insertPurchase);
-app.put('/api/purchase/:id', db.updatePurchase);
-app.delete('/api/purchase/:id', db.DeletePurchase);
+            app.get('/api/purchase', db.getPurchase);
+            app.get('/api/purchase/:id', db.getPurchaseByID);
+            app.post('/api/purchase', db.insertPurchase);
+            app.put('/api/purchase/:id', db.updatePurchase);
+            app.delete('/api/purchase/:id', db.DeletePurchase);
 
 
 var port = process.env.PORT || 8080;
