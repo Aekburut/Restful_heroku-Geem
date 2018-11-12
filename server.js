325e6e6a0aa0a603a4649e8c2618ed61c751faa4
Aekburut Rawangngan <egburut@gmail.com>
2018-11-12 14:47:47 +0700

    แก้ไข data

--- server.js
+++ server.js
@@ -25,29 +25,45 @@ app.get('/api/json', function (req, res) {
 
 //การทำงานของ Products
             app.get('/api/products', db.getAllProducts);
+            
             app.get('/api/products/:id', db.getProductsByID);
+            
             app.post('/api/products', db.InsertProducts);
+            
             app.put('/api/products/:id', db.UpdateProducts);
+            
             app.delete('/api/products/:id', db.DeleteProducts);
 
 //การทำงานของ users
             app.get('/api/users', db.getUsers);
+            
             app.get('/api/users/:id', db.getUsersByID);
+            
             app.post('/api/users', db.InsertUsers);
+            
             app.put('/api/users/:id', db.UpdateUsers);
+            
             app.delete('/api/users/:id', db.DeleteUsers);
 
 //การทำงานของ Purchase_item
             app.get('/api/purchase_items', db.getPurchase_items);
+            
             app.get('/api/purchase_items/:id', db.getPurchase_itemsByID);
+            
             app.post('/api/purchase_items', db.InsertPurchase_items);
+            
             app.put('/api/purchase_items/:id', db.UpdatePurchase_items);
+            
             app.delete('/api/purchase_items/:id', db.DeletePurchase_items);
 //การทำงานของ Purchase
             app.get('/api/purchase', db.getPurchase);
+            
             app.get('/api/purchase/:id', db.getPurchaseByID);
+            
             app.post('/api/purchase', db.insertPurchase);
+            
             app.put('/api/purchase/:id', db.updatePurchase);
+            
             app.delete('/api/purchase/:id', db.DeletePurchase);
 
 
