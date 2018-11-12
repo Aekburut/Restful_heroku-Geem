9e1c3be2b9f29c67bc12371946d559d4e30f1b71
Aekburut Rawangngan <egburut@gmail.com>
2018-11-12 14:13:27 +0700

    สร้าง function

--- server.js
+++ server.js
@@ -1,38 +1,55 @@
 var express = require('express');
 var app = express();
-var db = require('./database')
-
+var db = require('./database');
+var cors = require('cors')
+app.use(cors())
 var bodyParser = require('body-parser');
-
-app.use(bodyParser.json()); //รับ json เข้ามา
+app.use(bodyParser.json());//สำคํญต้องใช้
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
-// Add routing
 // index page
-
 app.get('/', function (req, res) {
-    res.send('Express is running');
+    res.send('REST is running');
 });
 
 
 var output = {
-    status: 'success',
-    message: 'REST API is working'
+    status: 'succes',
+    message: 'rest api is working'
 }
-
-
 app.get('/api/json', function (req, res) {
-    res.status(500).json(output);
+    res.status(200).json(output);
 
 });
 
-app.get('/api/products/', db.getAllProducts);
-app.get('/api/products/:id', db.getProductByID);
-app.post('/api/products', db.insertProduct);
-app.put('/api/products/:id', db.updateProduct);
-app.delete('/api/products/:id', db.deleteProduct);
+//Products
+app.get('/api/products', db.getAllProducts);
+app.get('/api/products/:id', db.getProductsByID);
+app.post('/api/products', db.InsertProducts);
+app.put('/api/products/:id', db.UpdateProducts);
+app.delete('/api/products/:id', db.DeleteProducts);
+
+//users
+app.get('/api/users', db.getUsers);
+app.get('/api/users/:id', db.getUsersByID);
+app.post('/api/users', db.InsertUsers);
+app.put('/api/users/:id', db.UpdateUsers);
+app.delete('/api/users/:id', db.DeleteUsers);
+
+//Purchase_item
+app.get('/api/purchase_items', db.getPurchase_items);
+app.get('/api/purchase_items/:id', db.getPurchase_itemsByID);
+app.post('/api/purchase_items', db.InsertPurchase_items);
+app.put('/api/purchase_items/:id', db.UpdatePurchase_items);
+app.delete('/api/purchase_items/:id', db.DeletePurchase_items);
+//Purchase
+app.get('/api/purchase', db.getPurchase);
+app.get('/api/purchase/:id', db.getPurchaseByID);
+app.post('/api/purchase', db.insertPurchase);
+app.put('/api/purchase/:id', db.updatePurchase);
+app.delete('/api/purchase/:id', db.DeletePurchase);
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
